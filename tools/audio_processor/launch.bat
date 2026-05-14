e22ab0759e3080b2d4376cfebd5a64503223f2fa
Yixin Zhang <yz696@duke.edu>
2026-05-14 03:17:53 -0400

    audio utils and GUI

---
+++ tools/audio_processor/launch.bat
@@ -0,0 +1,3 @@
+@echo off
+set PYTHONPATH=C:\Users\82755\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\LocalCache\local-packages\Python312\site-packages
+"C:\Users\82755\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\pythonw.exe" "%~dp0app.py" %*
